8dd3b5f632a4e64dccb2690d7db1ad4e7fb5fbac
ciesielski-filip <filipciesielski2005@gmail.com>
2023-03-19 12:14:58 +0100

    update js and css files

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
 function article1() {
-    document.getElementById("btnArt1").classList.add("hover")
-    document.getElementById("btnArt2").classList.remove("hover")
-    document.getElementById("btnArt3").classList.remove("hover")
-    document.getElementById("btnArt4").classList.remove("hover")
+    document.getElementById("btnArt1").classList.add("colorBtnHover")
+    document.getElementById("btnArt2").classList.remove("colorBtnHover")
+    document.getElementById("btnArt3").classList.remove("colorBtnHover")
+    document.getElementById("btnArt4").classList.remove("colorBtnHover")
     document.getElementById("art1").style.display = "inline"
     document.getElementById("art2").style.display = "none"
     document.getElementById("art3").style.display = "none"
     document.getElementById("art4").style.display = "none"
 }
 function article2() {
-    document.getElementById("btnArt1").classList.remove("hover")
-    document.getElementById("btnArt2").classList.add("hover")
-    document.getElementById("btnArt3").classList.remove("hover")
-    document.getElementById("btnArt4").classList.remove("hover")
+    document.getElementById("btnArt1").classList.remove("colorBtnHover")
+    document.getElementById("btnArt2").classList.add("colorBtnHover")
+    document.getElementById("btnArt3").classList.remove("colorBtnHover")
+    document.getElementById("btnArt4").classList.remove("colorBtnHover")
     document.getElementById("art1").style.display = "none"
     document.getElementById("art2").style.display = "inline"
     document.getElementById("art3").style.display = "none"
     document.getElementById("art4").style.display = "none"
 }
 function article3() {
-    document.getElementById("btnArt1").classList.remove("hover")
-    document.getElementById("btnArt2").classList.remove("hover")
-    document.getElementById("btnArt3").classList.add("hover")
-    document.getElementById("btnArt4").classList.remove("hover")
+    document.getElementById("btnArt1").classList.remove("colorBtnHover")
+    document.getElementById("btnArt2").classList.remove("colorBtnHover")
+    document.getElementById("btnArt3").classList.add("colorBtnHover")
+    document.getElementById("btnArt4").classList.remove("colorBtnHover")
     document.getElementById("art1").style.display = "none"
     document.getElementById("art2").style.display = "none"
     document.getElementById("art3").style.display = "inline"
     document.getElementById("art4").style.display = "none"
 }
 function article4() {
-    document.getElementById("btnArt1").classList.remove("hover")
-    document.getElementById("btnArt2").classList.remove("hover")
-    document.getElementById("btnArt3").classList.remove("hover")
-    document.getElementById("btnArt4").classList.add("hover")
+    document.getElementById("btnArt1").classList.remove("colorBtnHover")
+    document.getElementById("btnArt2").classList.remove("colorBtnHover")
+    document.getElementById("btnArt3").classList.remove("colorBtnHover")
+    document.getElementById("btnArt4").classList.add("colorBtnHover")
     document.getElementById("art1").style.display = "none"
     document.getElementById("art2").style.display = "none"
     document.getElementById("art3").style.display = "none"
