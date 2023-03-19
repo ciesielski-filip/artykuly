8418faa8fe3e03e86306691fe7787ca16920bd0a
ciesielski-filip <filipciesielski2005@gmail.com>
2023-03-19 10:35:43 +0100

    Add html css and js

---
+++ script.js
@@ -0,0 +1,24 @@
+function article1() {
+    document.getElementById("art1").style.display = "inline"
+    document.getElementById("art2").style.display = "none"
+    document.getElementById("art3").style.display = "none"
+    document.getElementById("art4").style.display = "none"
+}
+function article2() {
+    document.getElementById("art1").style.display = "none"
+    document.getElementById("art2").style.display = "inline"
+    document.getElementById("art3").style.display = "none"
+    document.getElementById("art4").style.display = "none"
+}
+function article3() {
+    document.getElementById("art1").style.display = "none"
+    document.getElementById("art2").style.display = "none"
+    document.getElementById("art3").style.display = "inline"
+    document.getElementById("art4").style.display = "none"
+}
+function article4() {
+    document.getElementById("art1").style.display = "none"
+    document.getElementById("art2").style.display = "none"
+    document.getElementById("art3").style.display = "none"
+    document.getElementById("art4").style.display = "inline"
+}
