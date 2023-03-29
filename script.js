5a4bd494a15c87b091808d6a5b945d483a1f97e6
ciesielski-filip <filipciesielski2005@gmail.com>
2023-03-29 21:46:59 +0200

    .

--- script.js
+++ script.js
@@ -5,12 +5,13 @@ function showArticle(articleID, buttonID) {
     document.querySelectorAll(".colorBtnHover").forEach(function(button) {
         button.classList.remove("colorBtnHover")
     })
-    document.getElementById(buttonID).classList.add("colorBtnHover")
     document.querySelectorAll(".art").forEach(function(art) {
         art.style.display = "none"
     })
+    document.getElementById(buttonID).classList.add("colorBtnHover")
     document.getElementById(articleID).style.display = "inline"
 }
+
 document.getElementById("btnArt1").addEventListener("click", function() {
     showArticle("art1", "btnArt1")
 })
@@ -26,3 +27,44 @@ document.getElementById("btnArt3").addEventListener("click", function() {
 document.getElementById("btnArt4").addEventListener("click", function() {
     showArticle("art4", "btnArt4")
 })
+
+// function article1() {
+//     document.getElementById("btnArt1").classList.add("colorBtnHover")
+//     document.getElementById("btnArt2").classList.remove("colorBtnHover")
+//     document.getElementById("btnArt3").classList.remove("colorBtnHover")
+//     document.getElementById("btnArt4").classList.remove("colorBtnHover")
+//     document.getElementById("art1").style.display = "inline"
+//     document.getElementById("art2").style.display = "none"
+//     document.getElementById("art3").style.display = "none"
+//     document.getElementById("art4").style.display = "none"
+// }
+// function article2() {
+//     document.getElementById("btnArt1").classList.remove("colorBtnHover")
+//     document.getElementById("btnArt2").classList.add("colorBtnHover")
+//     document.getElementById("btnArt3").classList.remove("colorBtnHover")
+//     document.getElementById("btnArt4").classList.remove("colorBtnHover")
+//     document.getElementById("art1").style.display = "none"
+//     document.getElementById("art2").style.display = "inline"
+//     document.getElementById("art3").style.display = "none"
+//     document.getElementById("art4").style.display = "none"
+// }
+// function article3() {
+//     document.getElementById("btnArt1").classList.remove("colorBtnHover")
+//     document.getElementById("btnArt2").classList.remove("colorBtnHover")
+//     document.getElementById("btnArt3").classList.add("colorBtnHover")
+//     document.getElementById("btnArt4").classList.remove("colorBtnHover")
+//     document.getElementById("art1").style.display = "none"
+//     document.getElementById("art2").style.display = "none"
+//     document.getElementById("art3").style.display = "inline"
+//     document.getElementById("art4").style.display = "none"
+// }
+// function article4() {
+//     document.getElementById("btnArt1").classList.remove("colorBtnHover")
+//     document.getElementById("btnArt2").classList.remove("colorBtnHover")
+//     document.getElementById("btnArt3").classList.remove("colorBtnHover")
+//     document.getElementById("btnArt4").classList.add("colorBtnHover")
+//     document.getElementById("art1").style.display = "none"
+//     document.getElementById("art2").style.display = "none"
+//     document.getElementById("art3").style.display = "none"
+//     document.getElementById("art4").style.display = "inline"
+// }
